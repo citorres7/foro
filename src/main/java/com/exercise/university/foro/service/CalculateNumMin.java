92a041de629d23ef4f87f48fa5cbbb3cefc905a1
citorres7 <citorres7@poligran.edu.co>
2024-03-19 22:52:15 -0500

    git commit "cambio en idioma anunciados"

--- src/main/java/com/exercise/university/foro/service/CalculateNumMin.java
+++ src/main/java/com/exercise/university/foro/service/CalculateNumMin.java
@@ -7,22 +7,22 @@ public class CalculateNumMin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese 5 números enteros separados por espacios:");
+        System.out.println("Enter 5 whole numbers separated by spaces:");
 
-        int[] numeros = new int[5];
+        int[] numbers = new int[5];
         for (int i = 0; i < 5; i++) {
-            numeros[i] = scanner.nextInt();
+            numbers[i] = scanner.nextInt();
         }
 
-        int menor = numeros[0];
+        int minor = numbers[0];
 
-        for (int i = 1; i < numeros.length; i++) {
-            if (numeros[i] < menor) {
-                menor = numeros[i];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < minor) {
+                minor = numbers[i];
             }
         }
 
-        System.out.println("El menor de los números es: " + menor);
+        System.out.println("The smallest number is: " + minor);
         scanner.close();
     }
 }
